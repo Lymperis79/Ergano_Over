9e03846ea5608c2e39c1dc0680a37e31c1781267
Lymperis79 <58636259+Lymperis79@users.noreply.github.com>
2026-09-18 21:31:13 +0300

    Delete src/ErganiManager.Core directory

--- src/ErganiManager.Core/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-// Global using directives for ErganiManager.Core.
-// Applied to every .cs file in this project automatically.
-global using System.Linq;
--- src/ErganiManager.Core/Interfaces/IPasswordHasher.cs
+++
@@ -1,7 +0,0 @@
-namespace ErganiManager.Core.Interfaces;
-
-public interface IPasswordHasher
-{
-    string Hash(string plainTextPassword);
-    bool Verify(string plainTextPassword, string hash);
-}
